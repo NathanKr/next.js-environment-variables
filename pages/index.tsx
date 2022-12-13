84767a498cc7f78441aeb286f0d297913d4314aa
Nathan Krasney <natankrasney@gmail.com>
2022-12-13 09:37:18 +0200

    readme

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,8 @@ import styles from '../styles/Home.module.css'
 export default function Home() {
   return (
     <div className={styles.container}>
-     ----<p>{process.env.VERCEL_URL}</p>----
+      <p>You can not access from client , access from server</p>
+    <p>process.env.VERCEL_URL : {process.env.VERCEL_URL}</p>
     </div>
   )
 }
