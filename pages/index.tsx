5e3771e2c5540ab9f723d54d1597368e1de282b9
Nathan Krasney <natankrasney@gmail.com>
2023-01-22 13:06:48 +0200

    ...

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,15 @@
+import { useEffect, useState } from "react";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
+  const [vercelUrl, setVercelUrl] = useState<string | undefined>();
+  const [envKey, setEnvKey] = useState<string | undefined>();
+
+  useEffect(() => {
+    setVercelUrl(process.env.NEXT_PUBLIC_VERCEL_ENV);
+    setEnvKey(process.env.NEXT_PUBLIC_ENV_KEY)
+  }, []);
+
   const msg =
     process.env.NODE_ENV === "development"
       ? "This is development so vercel environment variables on development are empty"
@@ -8,15 +17,9 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <h3>{msg}</h3>
-      <p>
-        process.env.NEXT_PUBLIC_VERCEL_ENV :{process.env.NEXT_PUBLIC_VERCEL_ENV}
-      </p>
-      <p>
-        process.env.NEXT_PUBLIC_VERCEL_URL :{process.env.NEXT_PUBLIC_VERCEL_URL}
-      </p>
+      <p>process.env.NEXT_PUBLIC_VERCEL_URL :{vercelUrl}</p>
       <h3>ENV_KEY is : 1 for production , 2 for preview , 3 for development</h3>
-      <p>process.env.NEXT_PUBLIC_ENV_KEY : {process.env.NEXT_PUBLIC_ENV_KEY}</p>
-      <p>process.env.ENV_KEY : {process.env.ENV_KEY}</p>
+      <p>process.env.NEXT_PUBLIC_ENV_KEY : {envKey}</p>
     </div>
   );
 }
