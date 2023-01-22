25af8090e1d97d7bb93dc3a715afaf97af18cb89
Nathan Krasney <natankrasney@gmail.com>
2023-01-22 09:45:57 +0200

    ...

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,14 +1,13 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from "next";
 
 type Data = {
-  name: string
-}
+  vercel_url: string;
+};
 
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  
-  res.status(200).json({ name: process.env.VERCEL_URL??"" })
+  res.status(200).json({ vercel_url: process.env.VERCEL_URL ?? "" });
 }
