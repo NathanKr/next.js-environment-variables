e023a10fe3104352711ad27362017978edc7fcaf
Nathan Krasney <natankrasney@gmail.com>
2023-01-22 13:55:40 +0200

    ...

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,6 @@ import { useEffect, useState } from "react";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
-  const [envKey , setEnvKey]=useState<string | undefined>()
   const msg =
     process.env.NODE_ENV === "development"
       ? "This is development so vercel environment variables on development are empty"
@@ -10,12 +9,17 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <h3>{msg}</h3>
-      <p>process.env.NEXT_PUBLIC_VERCEL_URL :{process.env.NEXT_PUBLIC_VERCEL_URL}</p>
-      <p>process.env.NEXT_PUBLIC_VERCEL_ENV :{process.env.NEXT_PUBLIC_VERCEL_ENV}</p>
-      <h3>ENV_KEY is : 1 for production , 2 for preview , 3 for development</h3>
+      <p>
+        process.env.NEXT_PUBLIC_VERCEL_URL :{process.env.NEXT_PUBLIC_VERCEL_URL}
+      </p>
+      <p>
+        process.env.NEXT_PUBLIC_VERCEL_ENV :{process.env.NEXT_PUBLIC_VERCEL_ENV}
+      </p>
+      <h3>
+        ENV_KEY is : 1 for production , 2 for preview , 3 for development{" "}
+        <span style={{color:'red'}}>but it is not working on the browser</span>
+      </h3>
       <p>process.env.NEXT_PUBLIC_ENV_KEY : {process.env.NEXT_PUBLIC_ENV_KEY}</p>
-      <button onClick={() => setEnvKey(process.env.NEXT_PUBLIC_ENV_KEY)}>Click</button>
-      <p>envKey : {envKey}</p>
     </div>
   );
 }
