ed19b6c2c792627dd3d16e3c3891b04953b9bf42
Nathan Krasney <natankrasney@gmail.com>
2023-01-22 10:19:03 +0200

    ...

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,12 @@
-import styles from '../styles/Home.module.css'
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
     <div className={styles.container}>
-      <p style={{color:'red'}}>You can not access from client , access from api : /api/hello</p>
-    <p>process.env.VERCEL_URL : {process.env.VERCEL_URL}</p>
+      <p style={{ color: "red" }}>
+        You can not access from client , access from api : /api/hello
+      </p>
+      <p>process.env.NEXT_PUBLIC_VERCEL_URL : {process.env.NEXT_PUBLIC_VERCEL_URL}</p>
     </div>
-  )
+  );
 }
