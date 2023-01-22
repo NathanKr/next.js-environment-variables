3ae4a7f3214c849677ae63f930be4dcda70ca061
Nathan Krasney <natankrasney@gmail.com>
2023-01-22 10:41:59 +0200

    ...

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import styles from "../styles/Home.module.css";
 
 export default function Home() {
   const msg =
-    process.env.NODE_ENV === "development"
+    process.env.NODE_ENV === "development" 
       ? "This is development so vercel environment variables on development are empty"
       : "This is production\\preview so vercel environment variables should not be empty";
   return (
