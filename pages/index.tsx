69736e0fc5f26dc81267d9b9c7ae23b3bfb50cad
Nathan Krasney <natankrasney@gmail.com>
2023-01-22 20:16:14 +0200

    ...

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,7 +7,7 @@ export default function Home() {
       : "This is production\\preview so vercel environment variables should not be empty";
   return (
     <div className={styles.container}>
-      <h1>dev_22_1_23</h1>
+      <h1>dev_22_1_23 !!!!!!!!!!!!!</h1>
       <p>{msg}</p>
       <p>
         process.env.NEXT_PUBLIC_VERCEL_ENV :{process.env.NEXT_PUBLIC_VERCEL_ENV}
